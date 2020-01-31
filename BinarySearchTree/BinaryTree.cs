6c6ee5e731e318a17a76e686b8cb98786e67a23a
David steinhafel <davidsteinhafel@outlook.com>
2020-01-31 11:29:48 -0600

    finished binary tree project.

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        Node current;
+        Node root;
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void InsertNode(int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+            }
+            else
+            {
+                current = root;
+
+                while (current != null)
+                {
+
+                    if (data >= current.data)
+                    {
+                        if (current.rightChild == null)
+                        {
+                            current.rightChild = new Node(data);
+                            return;
+                        }
+                        else
+                        {
+                            current = current.rightChild;
+                        }
+                    }
+                    if (data < current.data)
+                    {
+                        if (current.leftChild == null)
+                        {
+                            current.leftChild = new Node(data);
+                            return;
+                        }
+                        else
+                        {
+                            current = current.leftChild;
+                        }
+                    }
+
+                }
+            }
+        }
+        public bool SearchTree(int data)
+        {
+            
+            if (root == null)
+            {
+                return false;
+            }
+            else
+            {
+                current = root;
+            }
+            while (current != null)
+            {
+
+                if (data > current.data)
+                {
+                    if (current.rightChild == null)
+                    {
+
+                        return false;
+                    }
+                    else
+                    {
+                        current = current.rightChild;
+                    }
+                }
+                else if (data < current.data)
+                {
+                    if (current.leftChild == null)
+                    {
+
+                        return false;
+                    }
+                    else
+                    {
+                        current = current.leftChild;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
+
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.InsertNode(75);
+            binaryTree.InsertNode(42);
+            binaryTree.InsertNode(32);
+            binaryTree.InsertNode(67);
+            binaryTree.InsertNode(54);
+            binaryTree.InsertNode(32);
+            Console.WriteLine(binaryTree.SearchTree(67));
+            Console.ReadLine();
+        }
+    }
+}
